7c28971bfee74bc82890717b1380823ec96c1c6c
jorgeprueba <jgamarra@misena.edu.co>
2020-08-30 06:00:06 -0500

    Se agrego toast

--- app/src/main/java/com/example/myapplicationformulario2/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationformulario2/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.myapplicationformulario2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buttonGuardar.setOnClickListener {
+            val usuario = textInputUsuario.editText?.text.toString()
+            
+
+            if (usuario.isEmpty()){
+
+                Toast.makeText(this, "El campo no puede estar vacio", Toast.LENGTH_SHORT).show()
+
+            }
+        }
 
     }
 }
